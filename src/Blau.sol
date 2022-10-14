96183b3f7c92c20a4a262ff28d60c3567b9272b1
Joe Blau <im@joeblau.com>
2022-10-14 14:11:46 -0700

    feat: add core functions

--- src/Blau.sol
+++ src/Blau.sol
@@ -7,4 +7,12 @@ contract Blau is ERC20 {
     constructor() ERC20("BLAU", "BLAU", 18) {
         _mint(msg.sender, 0);
     }
+
+    function startStake(address burnAddress, uint256 term) public {}
+
+    function startStake(uint256 amount, uint256 term) public {}
+
+    function deferStake() public {}
+
+    function endStake() public {}
 }
