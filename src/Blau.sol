a88c12e5484974b8ce06c75414663cd5935e0383
Joe Blau <im@joeblau.com>
2022-10-14 20:18:08 -0700

    feat: add calculation to update share rate

--- src/Blau.sol
+++ src/Blau.sol
@@ -4,12 +4,22 @@ pragma solidity ^0.8.13;
 import {PRBMathUD60x18} from "prb-math/PRBMathUD60x18.sol";
 import {ERC20} from "solmate/tokens/ERC20.sol";
 
+struct Stake {
+    uint256 startTs;
+    uint256 term;
+    uint256 base;
+    uint256 bonus;
+}
+
 contract Blau is ERC20("BLAU", "BLAU", 18) {
     using PRBMathUD60x18 for uint256;
 
-    uint256 internal constant EQUITY_SCALE = 1e5;
-    uint256 internal constant SIZE_BONUS_PERCENT = 10;
-    uint256 internal constant TIME_BONUS_PERCENT = 20;
+    uint256 internal constant DECIMALS = 18;
+    uint256 internal constant SHARE_RATE_SCALE = 1e5;
+    uint256 internal constant TIME_BONUS = 1_820;
+    uint256 internal constant MIN_BONUS = 1e19;
+
+    uint256 public shareRate = 1e18;
 
     function startStake(address burnAddress, uint256 term) public {}
 
@@ -20,7 +30,7 @@ contract Blau is ERC20("BLAU", "BLAU", 18) {
     function endStake() public {}
 
     function calculateBase(uint256 xen) public pure returns (uint256) {
-        return xen.ln();
+        return xen.ln() * 10**DECIMALS;
     }
 
     function calculateBonus(uint256 xen, uint256 stakeDays)
@@ -29,8 +39,20 @@ contract Blau is ERC20("BLAU", "BLAU", 18) {
         returns (uint256)
     {
         uint256 base = calculateBase(xen);
-        uint256 timeBonus = base * stakeDays * TIME_BONUS_PERCENT;
-        uint256 sizeBonus = base * SIZE_BONUS_PERCENT;
-        return (timeBonus + sizeBonus) / 100;
+        uint256 timeBonus = (base * stakeDays) / TIME_BONUS;
+        if (base > MIN_BONUS) {
+            uint256 sizeBonus = base.ln();
+            return timeBonus + sizeBonus;
+        } else {
+            return timeBonus;
+        }
+    }
+
+    function _updateEquity(Stake memory stake) public {
+        uint256 roi = 1e18 + stake.bonus.div(stake.base);
+
+        if (roi > shareRate) {
+            shareRate = roi;
+        }
     }
 }
--- test/Blau.t.sol
+++ test/Blau.t.sol
@@ -3,9 +3,9 @@ pragma solidity ^0.8.13;
 
 import {Test} from "forge-std/Test.sol";
 import {console} from "forge-std/console.sol";
-import {Blau} from "src/Blau.sol";
+import {Blau, Stake} from "src/Blau.sol";
 
-contract CounterTest is Test {
+contract BlauTest is Test {
     Blau internal BLAU;
 
     /// ============ Setup test suite ============
@@ -24,27 +24,56 @@ contract CounterTest is Test {
 
     /// @notice Test function that caluclates base amount from burn
     function testCalculateBase() public {
-        uint256 burn1xen = 1 * 10**18;
+        uint256 burn1xen = 1e18;
         uint256 ln1xen = BLAU.calculateBase(burn1xen);
         assertEq(ln1xen, 0); // verify ln(1) = 0
 
-        uint256 burn10xen = 10 * 10**18;
+        uint256 burn10xen = 1e19;
         uint256 ln10xen = BLAU.calculateBase(burn10xen);
-        assertEq(ln10xen, 2302585092994045674); // verify ln(10)
+        assertEq(ln10xen, 2302585092994045674000000000000000000); // verify ln(10)
 
-        uint256 burn100xen = 100 * 10**18;
+        uint256 burn100xen = 1e20;
         uint256 ln100xen = BLAU.calculateBase(burn100xen);
-        assertEq(ln100xen, 4605170185988091359); // verify ln(100)
+        assertEq(ln100xen, 4605170185988091359000000000000000000); // verify ln(100)
     }
 
-    /// @notice Test calculating bonus amount from burn and stake days
-    function testCalculateBonus() public {
-        uint256 burn1xen = 1 * 10**18;
+    /// @notice Test calculating size bonus
+    function testCalculateSizeBonus() public {
+        uint256 burn1xen = 1 * 1e18;
         uint256 bonus1Xen = BLAU.calculateBonus(burn1xen, 1);
         assertEq(bonus1Xen, 0); // verify 1 xen stake no bonus
 
-        uint256 burn2xen = 2 * 10**18;
+        uint256 burn2xen = 2 * 1e18;
         uint256 bonus2Xen = BLAU.calculateBonus(burn2xen, 1);
-        assertEq(bonus2Xen, 207944154167983592); // verify 2 xen stake bonus
+        assertEq(bonus2Xen, 380850099208802238809962102366776); // verify 2 xen stake bonus
+
+        uint256 burn3xen = 3 * 1e18;
+        uint256 bonus3Xen = BLAU.calculateBonus(burn3xen, 1);
+        assertEq(bonus3Xen, 603633125641860046074007004026814); // verify 2 xen stake bonus
+    }
+
+    /// @notice Test calculating time bonus
+    function testCalculateTimeBonus() public {
+        uint256 burnxen = 3 * 1e18;
+        uint256 bonus356Days = BLAU.calculateBonus(burnxen, 365);
+        assertEq(bonus356Days, 220326090859263796046074007004026814); // verify 1 xen stake no bonus
+
+        uint256 bonus3560Days = BLAU.calculateBonus(burnxen, 3650);
+        assertEq(bonus3560Days, 2203260908592637586595524556454576265); // verify 2 xen stake bonus
+
+        uint256 bonus35600Days = BLAU.calculateBonus(burnxen, 36500);
+        assertEq(bonus35600Days, 22032609085926375492090030050960070770); // verify 2 xen stake bonus
+    }
+
+    /// @notice Test share rate update
+    function testShareReateUpdate() public {
+        uint256 base = 13.81551 * 1e18;
+        uint256 bonus = 2.77069 * 1e18;
+        Stake memory stake1 = Stake(0, 1, base, bonus);
+
+        assertEq(BLAU.shareRate(), 1000000000000000000); // verify initial share rate
+
+        BLAU._updateEquity(stake1);
+        assertEq(BLAU.shareRate(), 1200549237776962269); // verify 20% gain
     }
 }
